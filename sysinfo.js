d6773f92d8e5e378791d3c432ab26584b2f1b73e
Jonathan Niles <jonathanwniles@gmail.com>
2019-12-12 09:59:25 +0100

    chore: improved rendering of dashboard
    Improves the rendering of the system uptime and process uptime
    dashboard using human readable identifiers instead of number of seconds
    total.  Uses dayjs to accomplish this.

--- sysinfo.js
+++ sysinfo.js
@@ -1,16 +1,29 @@
 const process = require('process');
 const os = require('os');
+const dayjs = require('dayjs');
+dayjs.extend(require('dayjs/plugin/relativeTime'));
 
 function getSystemInfo() {
   const info = {};
+
+  // process uptime
+  const pUptime = Date.now() - (process.uptime() * 1000);
+  const pUptimeAgo = dayjs().from(dayjs(pUptime), true);
+
+  // system uptime
+  const sUptime = Date.now() - (os.uptime() * 1000);
+  const sUptimeAgo = dayjs().from(dayjs(sUptime), true);
+
   info.hostname = os.hostname();
   info.os = `${os.type()} - ${os.arch()}`;
   info.ram = (os.totalmem() / (1024 ** 3));
   info.cpus = os.cpus().length;
-  info.uptime = process.uptime();
-  info.sysuptime = os.uptime();
+
+  info.uptime = pUptimeAgo;
+  info.sysuptime = sUptimeAgo;
   info.loadavg = os.loadavg();
   info.nodeversion = process.versions.node;
+
   return info;
 }
 
