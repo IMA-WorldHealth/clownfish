692c2d6d41ae7a891b165ccf0c2a3e4b1ec4cf50
Jonathan Niles <jonathanwniles@gmail.com>
2020-03-23 11:42:54 +0100

    fix: add error handling to Google Drive linker

--- drive.js
+++ drive.js
@@ -1,18 +1,23 @@
 require('dotenv').config();
 const fs = require('fs');
-
-const TOKEN = JSON.parse(fs.readFileSync('./credentials/token.json'));
-const { installed } = JSON.parse(fs.readFileSync('./credentials/credentials.json'));
-
 const { google } = require('googleapis');
+const debug = require('debug')('clownfish:drive');
 
-const auth = new google.auth.OAuth2(
-  installed.client_id,
-  installed.client_secret,
-  installed.redirect_uris[0],
-);
+try {
+  const TOKEN = JSON.parse(fs.readFileSync('./credentials/token.json'));
+  const { installed } = JSON.parse(fs.readFileSync('./credentials/credentials.json'));
 
-// set the credentials for the drive API
-auth.setCredentials(TOKEN);
+  const auth = new google.auth.OAuth2(
+    installed.client_id,
+    installed.client_secret,
+    installed.redirect_uris[0],
+  );
 
-module.exports = google.drive({ version: 'v3', auth });
+  // set the credentials for the drive API
+  auth.setCredentials(TOKEN);
+
+  module.exports = google.drive({ version: 'v3', auth });
+} catch (e) {
+  debug('An error occurred loading google drive link.');
+  debug(e);
+}
