ab614185cef24aa7b7e8781b9b4ac974b24be192
Jonathan Niles <jonathanwniles@gmail.com>
2020-07-28 14:55:36 +0100

    feat: use imapflow to support a custom mail server
    Uses the imapflow library to support a custom mail server and remove the
    dependency on sendgrid.

--- schema.sql
+++ schema.sql
@@ -1,16 +1,43 @@
-CREATE TABLE IF NOT EXISTS logger (
-  googleDriveParentFolderId TEXT,
-  normalizedStructure TEXT,
-  normalizedReportName TEXT,
-  attachments TEXT,
-  start TEXT,
-  sender TEXT,
-  end TEXT,
-  elapsed TEXT
+/**
+Design:
+
+The `smtp` table keeps a record of each hostname which is the mail
+server.  It also contains the individual accounts and passwords, an
+the mapping to the google drive identifier.
+
+The `emails` table contains a key to the smtp table for the email address
+matched, and then contains ids to the message on the IMAP server and in
+google drive.
+
+Importantly, the `emails.seq` record must match the latest IMAP sequence
+from the folder.  It will be used for looking up new emails.
+
+The `envelope` is optional.  It contains the message body for posterity.
+
+*/
+
+-- SMTP credentials for different servers.  Each email host maps
+-- to it's own Google Drive folder.
+CREATE TABLE IF NOT EXISTS `smtp` (
+  id INTEGER PRIMARY KEY NOT NULL,
+  host TEXT NOT NULL, -- mail.domain.com
+  username TEXT NOT NULL, -- user@mail.domain.com
+  password TEXT NOT NULL,
+  gdrive_folder_id TEXT NOT NULL,
+  paused BOOLEAN NOT NULL DEFAULT FALSE,
+  UNIQUE (gdrive_folder_id, username) ON CONFLICT REPLACE
 );
 
-CREATE TABLE IF NOT EXISTS router (
-  gdrive_folder_id TEXT NOT NULL,
-  email_address TEXT NOT NULL,
-  UNIQUE (gdrive_folder_id, email_address) ON CONFLICT REPLACE
+-- inbox
+CREATE TABLE IF NOT EXISTS `inbox` (
+  smtp_id INTEGER NOT NULL,
+  seq INTEGER NOT NULL,
+  message_id TEXT NOT NULL,
+  from_address TEXT NOT NULL,
+  subject TEXT NOT NULL,
+  attachments TEXT NOT NULL,
+  date DATETIME NOT NULL,
+  gdrive_id TEXT NOT NULL,
+  timestamp DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  FOREIGN KEY (smtp_id) REFERENCES smtp (id)
 );
