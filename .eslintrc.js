2e16699b528d89c16444b45ddb468e95e712bcae
Jonathan Niles <jonathanwniles@gmail.com>
2021-06-29 14:51:41 +0100

    feat: replace gdrive with nextcloud
    This commit replaces gdrive with nextcloud, allowing us to resume
    uploads of bon de livraisons and rapport de retroinformation.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,15 @@
 module.exports = {
-    "env": {
-        "commonjs": true,
-        "es6": true,
-        "node": true
-    },
-    "extends": "airbnb-base",
-    "globals": {
-        "Atomics": "readonly",
-        "SharedArrayBuffer": "readonly"
-    },
-    "parserOptions": {
-        "ecmaVersion": 2018
-    },
-    "rules": {
-    }
+  env: {
+    commonjs: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  parserOptions: {
+    ecmaVersion: 12,
+  },
+  rules: {
+  },
 };
